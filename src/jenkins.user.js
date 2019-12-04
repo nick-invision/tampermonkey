54539ee334bc4cab3532815cbbb7ec3345937eda
Nick Fields <nickfields@invisionapp.com>
2019-12-03 23:33:32 -0500

    Added jenkins helper

---
+++ src/jenkins.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name            Jenkins Helpers
+// @author          Nick Fields
+// @description     Helpers to make you hate Jenkins a little less
+// @version         0.1
+// @updateURL       https://raw.githubusercontent.com/nick-invision/tampermonkey/master/jenkins.user.js
+// @downloadURL     https://raw.githubusercontent.com/nick-invision/tampermonkey/master/jenkins.user.js
+// @include         https://jenkins.*
+// @include         https://builds.*
+// ==/UserScript==
+function run() {
+  function getKeyCode(letter) {
+    return letter.charCodeAt(0);
+  }
+
+  // eslint-disable-next-line no-undef
+  jQuery(window).keydown((e) => {
+    if (e.shiftKey && e.ctrlKey) {
+      switch (e.which) {
+        case getKeyCode('R'): {
+          const elem = document.getElementById('workflow-editor-1');
+          elem.style.height = '1500px';
+          break;
+        }
+        default:
+          break;
+      }
+    }
+  });
+}
+run();
